36ddb4dfba320c4ec14c96c8d8ee626abb98e39f
Матвей <matvejsandrovskij@gmail.com>
2026-04-16 21:57:09 +0300

    feat: add CrateEmployeeCommand Commands

---
+++ src/ProjectManagement.Application/Commands/CreateEmployeeCommand.cs
@@ -0,0 +1,11 @@
+using ProjectManagement.Domain.ValueObjects;
+
+
+public class CreateEmployeeCommand
+{
+    public int EmployeeId { get; set; }
+    public string? EmployeeName;
+    public string? EmployeeEmail;
+    public string? EmployeeWorkPass;
+    public string? EmployeeRole;
+}
